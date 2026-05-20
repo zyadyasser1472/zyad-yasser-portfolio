97aa9c637ce3b4ea61641cf9fd1b7c7ec7c8c82b
zyadyasser1472 <130247739+zyadyasser1472@users.noreply.github.com>
2026-05-20 13:07:41 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+import type { NextConfig } from "next";
+
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isGithubActions ? "/zyad-yasser-portfolio" : "",
   images: {
     unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
   },
 };
 
